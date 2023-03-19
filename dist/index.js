242d9e384fb7101610901ad6f7f6cf876d472d42
wponc <poncyw@gmail.com>
2023-03-18 20:32:53 -0400

    changed three import

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-import * as THREE from '../node_modules/three/build/three.module.js';
+import * as THREE from 'https://unpkg.com/three@0.125.2/build/three.module.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
